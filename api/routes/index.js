fa1c05f8d9c96efe1743473148c547bcdd4cb706
IllusionMan1212 <hisham.abourgheba@gmail.com>
2020-12-12 23:57:04 +0200

    implement random, search, alphabetical, vegetarian routes

--- api/routes/index.js
+++ api/routes/index.js
@@ -6,8 +6,27 @@ const { JSDOM } = require('jsdom');
 
 const Cheese = require(`${__dirname}/../models/cheese`);
 
-router.get('/cheese', (req, res) => {
-    // TODO: random cheese
+router.get('/cheese/random', (req, res) => {
+    Cheese.nextCount((err, count) => {
+        if (err) {
+            res.status(500).json({ failed: true, status: 500, error: err });
+            return;
+        }
+
+        if (count) {
+            let random = Math.floor(Math.random() * (count + 1));
+            Cheese.find({ id: random })
+            .select('-_id')
+            .select('-__v')
+            .exec((err, cheese) => {
+                if (err) {
+                    res.status(500).json({ failed: true, status: 500, error: err });
+                    return;
+                }
+                res.status(200).json({ failed: false, status: 200, cheese: cheese[0] });
+            });
+        }
+    });
 });
 
 router.get('/cheese/search', (req, res) => {
@@ -15,19 +34,28 @@ router.get('/cheese/search', (req, res) => {
         res.status(400).json({ failed: true, status: 400, error: "Invalid or incomplete request" });
         return;
     }
-    if (req.query.q) {
-        // TODO: yes
-    }
+
+    let page = parseInt(req.query.page) || 1;
+    let per_page = parseInt(req.query.per_page) || 10;
+    
     if (req.query.by) {
         // TODO: either country, type, milk, texture or color, maybe some other shit
     }
-    if (req.query.per_page) {
-        // TODO: default to 20
-    }
-    if (req.query.page) {
-        // TODO: default to 1
-    }
-    res.status(400).json({ failed: true, status: 400, error: "Invalid or incomplete request" });
+
+    Cheese.find({ $text: { $search: req.query.q } })
+    .select("-_id")
+    .select("-__v")
+    .limit(per_page)
+    .skip((page - 1) * per_page)
+    .exec((err, cheeses) => {
+        if (err) {
+            res.status(500).json({ failed: true, status: 500, error: err });
+            return;
+        }
+        res.status(200).json({ failed: false, status: 200, cheeses: cheeses });
+        return;
+    });
+    // res.status(400).json({ failed: true, status: 400, error: "Invalid or incomplete request" });
 });
 
 router.get('/cheese/today', async (req, res) => {
@@ -42,24 +70,59 @@ router.get('/cheese/today', async (req, res) => {
     res.status(cheese_data.status).json(cheese_data);
 });
 
-router.get('/cheese/alphabetical', (req, res) => {// TODO: maybe use query strings, still haven't thought it out
-    // TODO: do custom per_page when using a db
-    let page = req.query.page || 1;
-    let per_page = req.query.per_page || 20;
+router.get('/cheese/alphabetical', (req, res) => {
+    let page = parseInt(req.query.page) || 1;
+    let per_page = parseInt(req.query.per_page) || 10;
+
+    if (per_page > 50) {
+        per_page = 50;
+    }
+
     if (req.query.letter) {
-        // TODO: query the db
+        Cheese.find({ name: { "$regex": "^" + req.query.letter, "$options": "i" } })
+        .select("-_id")
+        .select("-__v")
+        .limit(per_page)
+        .skip((page - 1) * per_page)
+        .exec((err, cheeses) => {
+            if (err) {
+                res.status(500).json({ failed: true, status: 500, error: err });
+                return;
+            }
+
+            res.status(200).json({ failed: false, status: 200, cheeses: cheeses });
+        });
         return;
     }
     res.status(400).json({ failed: true, status: 400, error: "Invalid or incomplete request" });
 });
 
-router.get('/cheese/vegetarian', (req, res) => {// TODO: maybe use query strings, still haven't thought it out
-    // TODO: fucking vegetarians making my life harder.
+router.get('/cheese/vegetarian', (req, res) => {
+    let page = parseInt(req.query.page) || 1;
+    let per_page = parseInt(req.query.per_page) || 10;
+
+    if (per_page > 50) {
+        per_page = 50;
+    }
+
+    Cheese.find({ "attributes.vegetarian": true })
+    .select('-_id')
+    .select('-__v')
+    .limit(per_page)
+    .skip((page - 1) * per_page)
+    .exec((err, cheeses) => {
+        if (err) {
+            res.status(500).json({ failed: true, status: 500, error: err });
+            return;
+        }
+        res.status(200).json({ failed: false, status: 200, cheeses: cheeses });
+    });
 });
 
 router.get("/cheese/scrape", async (req, res) => {
     let pages = 1;
     let all_cheeses = [];
+    let track = 0;
     for (let l = 0; l < 26; l++) {
         let letter = (l + 10).toString(36);
         let response = await axios.get(`https://cheese.com/alphabetical/?i=${letter}&per_page=100&page=1`);
@@ -68,7 +131,7 @@ router.get("/cheese/scrape", async (req, res) => {
         const ul = document.getElementById("id_page");
         if (ul) {
             let li_arr = ul.getElementsByTagName("li");
-            pages = li_arr[li_arr.length - 1].textContent;
+            pages = parseInt(li_arr[li_arr.length - 1].textContent);
         }
         for (let i = 1; i <= pages; i++) {
             let response = await axios.get(`https://cheese.com/alphabetical/?i=${letter}&per_page=100&page=${i}`)
@@ -80,22 +143,20 @@ router.get("/cheese/scrape", async (req, res) => {
                 all_cheeses.push(scraped_data.cheese);
                 console.log(scraped_data);
                 if (scraped_data.failed) {
-                    console.log(scraped_data.error);
+                    res.status(scraped_data.status).json(scraped_data);
                     return;
                 }
 
+                console.log(++track);
+
                 let new_cheese = new Cheese();
-                
                 new_cheese.name = scraped_data.cheese.cheese_name;
                 new_cheese.link = scraped_data.cheese.link;
                 new_cheese.image = scraped_data.cheese.image;
                 new_cheese.description = scraped_data.cheese.description;
                 new_cheese.attributes = scraped_data.cheese.attributes;
-                new_cheese.types = scraped_data.cheese.types;
-                new_cheese.countries = scraped_data.cheese.countries;
+                new_cheese.country_codes = scraped_data.cheese.country_codes;
                 new_cheese.milks = scraped_data.cheese.milks;
-                new_cheese.textures = scraped_data.cheese.textures;
-                new_cheese.colors = scraped_data.cheese.colors;
                 new_cheese.save(err => {
                     if (err) {
                         res.status(500).json({ failed: true, status: 500, error: err });
